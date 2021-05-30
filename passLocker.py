0a0a46e22e4e478afcf51ad5a913ca645d0fa876
ANGELA <angela.mbithe@student.moringaschool.com>
2021-05-30 21:07:36 +0300

    create user class

--- passLocker.py
+++ passLocker.py
@@ -1,2 +1,20 @@
-class lockerPass:
+class User:
+    """
+    a class that generates new instances of a user.
+    """
+    user_list = []
+
+    def __init__(self, firstname, lastname, password):
+        """
+        a method that defines the properties of a user.
+        """
+        self.firstname = firstname
+        self.lastname = lastname
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user to the user list
+        """
+        User.user_list.append(self)
     pass
