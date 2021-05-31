270602f63fce82cbe8e87136542138b41b8c0f39
ANGELA <angela.mbithe@student.moringaschool.com>
2021-05-31 11:18:05 +0300

    add user details class

--- passlocker.py
+++ passlocker.py
@@ -25,4 +25,40 @@ class User:
         '''
 
         User.user_list.remove(self)
-pass
+        
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+class user_details:
+    """
+    A class that creates new ojects of users details
+    """
+    userdetails_list = []
+
+    def __init__(self,account,username, password):
+        """
+        method that defines user details to be stored
+        """
+        self.account = account
+        self.userName = username
+        self.password = password
+
+    @classmethod
+    def check_user(cls,fisrstname, lastname, password):
+        """
+        method to check whether the user is in our user_list or not
+        """
+        find_user = ""
+        for user in User.user_list:
+            if(user.firstname== fisrstname and user.lastname== lastname and user.password == password):
+                    find_user == user.username
+        return find_user
+
+    def save_details(self):
+        """
+        method to store new details to the details list
+        """
+        user_details.userdetails_list.append(self)
+
+    pass
